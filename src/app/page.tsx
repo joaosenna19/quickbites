d4d56496a18ae2625f1c486d527c416a1119cf33
João Senna Radicchi <joaoradicchi@gmail.com>
2024-03-06 14:48:18 -0700

    add navigation and menu page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import { ReservationForm } from "@/components/reservation-form";
-import Image from "next/image";
+
 
 export default function Home() {
   return(
+    
     <ReservationForm/>
   );
 
---
+++ src/components/nav-bar.tsx
@@ -0,0 +1,27 @@
+
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+
+const NavBar = () => {
+  return (
+    <nav className="flex h-16 w-full items-center border-b border-gray-200 shrink-0 px-4 md:px-6">
+      <Link className="text-lg font-bold mr-6" href="#">
+        QuickBites
+      </Link>
+      <div className="flex-1" />
+      <Link href="/">
+      <Button className="h-10 w-20 mr-2 text-xs" size="sm" variant="outline">
+        Reservations
+      </Button>
+        </Link>
+        <Link href="/menu">
+      <Button className="h-10 w-20 text-xs" size="sm" variant="outline">
+        Menu
+      </Button>
+        </Link>
+    </nav>
+  )
+}
+
+export default NavBar;
+
